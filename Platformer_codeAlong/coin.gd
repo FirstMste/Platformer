46de8ad1283edc0f6c261bef8f4cd7d79424a2cd
FirstMste <84036016+FirstMste@users.noreply.github.com>
2022-11-17 19:52:47 +0000

    sounds

--- Platformer_codeAlong/coin.gd
+++ Platformer_codeAlong/coin.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 class_name Coin
+
 onready var animate = $AnimationPlayer
+onready var Sound = $Pickup
+
 var colected = false
 
 func _on_coin_body_entered(body):
 	if body is Player and !colected:
+		Sound.play()
 		body.coins += 1
 		colected = true
 		animate.play("collected")
